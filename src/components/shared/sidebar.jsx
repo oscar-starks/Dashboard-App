8d8f504d8d0c661af482b9f12bd3381a0c12c81e
Oscar Blessed <oscarblessed04@gmail.com>
2024-02-10 06:35:04 +0100

    sidebar modified

--- src/components/shared/sidebar.jsx
+++ src/components/shared/sidebar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {FcBullish} from 'react-icons/fc'
-import { DASHBOARD_SIDEBAR_LINKS } from '../lib/consts/navigation'
-import { Link } from 'react-router-dom'
+import { DASHBOARD_SIDEBAR_LINKS, DASHBOARD_SIDEBAR_BOTTOM_LINKS } from '../lib/consts/navigation'
+import { Link, useLocation } from 'react-router-dom'
+import classNames from 'classnames'
 
 const linkClasses = 'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
 
@@ -14,23 +15,26 @@ export default function Sidebar() {
             <span className='text-neutral-100 text-lg'> OpenShop</span>
         </div>
 
-        <div className='flex-1'>
+        <div className='flex-1 py-8 flex flex-col gap-0.5'>
             {DASHBOARD_SIDEBAR_LINKS.map((item) =>(
-                    <SidebarComponent item={item} key={item.key}/>
+                <SidebarComponent item={item} key={item.key}/>
             ))}
         </div>
 
-
-
-        <div>Bottom Part</div>
+        <div className='flex flex-col gap-0.5 pt-2 border-t border-neutral-700'>
+            {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item) =>(
+                <SidebarComponent item={item} key={item.key}/>
+            ))}
+        </div>
     </div>
   )
 }
 
 
 function SidebarComponent({item}){
+    const {pathname} = useLocation()
     return (
-        <Link to = {item.path} className={linkClasses}>
+        <Link to = {item.path} className={classNames(pathname===item.path?'text-white':"bg-neutral-700 text-neutral-400", linkClasses)}>
             <span className="text-xl">{item.icon}</span>
             {item.label}
         </Link>
